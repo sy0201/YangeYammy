de570919f5c81f59dd26e7409f027a4837b586c5
sy0201 <noey0201@gmail.com>
2024-04-12 08:32:27 +0900

    [ADD] : AlarmVC TabBar연결

---
+++ YangeYammy/YangeYammy/Screen/Alarm/AlarmViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AlarmViewController.swift
+//  YangeYammy
+//
+//  Created by siyeon park on 4/12/24.
+//
+
+import UIKit
+
+final class AlarmViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
---
+++ YangeYammy/YangeYammy/Screen/RootTabBarViewController.swift
@@ -0,0 +1,34 @@
+//
+//  RootTabBarViewController.swift
+//  YangeYammy
+//
+//  Created by siyeon park on 4/12/24.
+//
+
+import UIKit
+
+final class RootTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabBar()
+        setupTabBarItem()
+    }
+}
+
+// MARK: - Private Methods
+
+private extension RootTabBarViewController {
+    func setupTabBar() {
+        tabBar.barTintColor = .systemBackground
+        tabBar.tintColor = .black
+    }
+    
+    func setupTabBarItem() {
+        let alarmViewController = AlarmViewController()
+        alarmViewController.tabBarItem = UITabBarItem(title: "alarm",
+                                                      image: UIImage(systemName: "alarm"),
+                                                      selectedImage: UIImage(systemName: "alarm.fill"))
+        viewControllers = [alarmViewController]
+    }
+}
