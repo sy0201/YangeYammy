96571a1feafa5ef2de8ec110f3220f48f8f34942
sy0201 <noey0201@gmail.com>
2024-05-08 20:10:12 +0900

    [RENAME] : UIView Extension 추가

--- YangeYammy/YangeYammy/Util/Extension/UIView.swift
+++ YangeYammy/YangeYammy/Util/Extension/UIView.swift
@@ -12,3 +12,16 @@ extension UIView {
         views.forEach { self.addSubview($0) }
     }
 }
+
+extension UIView {
+    var parentViewController: UIViewController? {
+        var parentResponder: UIResponder? = self
+        while let responder = parentResponder {
+            if let viewController = responder as? UIViewController {
+                return viewController
+            }
+            parentResponder = responder.next
+        }
+        return nil
+    }
+}
