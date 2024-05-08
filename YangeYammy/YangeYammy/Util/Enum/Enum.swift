3280581da21957154aa0d0a4937090c2372435c5
sy0201 <noey0201@gmail.com>
2024-05-08 20:09:59 +0900

    [RENAME] : Enum 구조체로 수정 및 추가

--- YangeYammy/YangeYammy/Util/Enum/Enum.swift
+++ YangeYammy/YangeYammy/Util/Enum/Enum.swift
@@ -7,13 +7,25 @@
 
 import Foundation
 
-enum Day: String, CaseIterable {
-    case sunday = "일요일 마다"
-    case monday = "월요일 마다"
-    case tuesday = "화요일 마다"
-    case wednesday = "수요일 마다"
-    case thursday = "목요일 마다"
-    case friday = "금요일 마다"
-    case saturday = "토요일 마다"
-    case none = "안함"
+struct Enum {
+    enum Day: String, CaseIterable {
+        case sunday = "일요일 마다"
+        case monday = "월요일 마다"
+        case tuesday = "화요일 마다"
+        case wednesday = "수요일 마다"
+        case thursday = "목요일 마다"
+        case friday = "금요일 마다"
+        case saturday = "토요일 마다"
+        case none = "안함"
+    }
+    
+    enum Gender: String {
+        case male = "남아"
+        case female = "여아"
+    }
+    
+    enum Neutrification: String {
+        case yes = "중성화후"
+        case no = "중성화전"
+    }
 }
